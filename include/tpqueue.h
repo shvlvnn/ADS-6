fafded5b842ec05177b945d1c6d982afb4c2b362
Anna Shevelyova <99125817+shvlvnn@users.noreply.github.com>
2022-04-07 23:41:54 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,15 +1,27 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
-
 template<typename T, int size>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
+ private:
+  T arr[size] = { 0 };
+  int a;
+  int b;
+ public:
+  void push(T x) {
+    int k = b;
+    for (; (k >= a) && arr[k % size].prior < x.prior; --k) {
+      arr[(k+1) % size] = arr[k % size];
+    }
+    arr[(k + 1) % size] = x;
+    ++b;
+  }
+  T pop() {
+    return arr[(a++) % size];
+  }
 };
-
 struct SYM {
   char ch;
   int prior;
 };
-
 #endif  // INCLUDE_TPQUEUE_H_
